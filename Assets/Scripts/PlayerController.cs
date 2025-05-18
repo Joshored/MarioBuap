950f66fa151b1cbd8737607fab451053970943c0
Joshored <jorgelpr.lh@gmail.com>
2025-05-18 14:09:51 -0600

    Commit inicial del proyecto

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool isGrounded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+
+        // Movimiento
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Saltar
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+
+        // Animaciones
+        if (Mathf.Abs(moveInput) > 0.01f)
+        {
+            animator.Play("Mario_Walk"); // Usa el nombre exacto de tu animación de caminar
+        }
+        else
+        {
+            animator.Play("Mario_Idle"); // Usa el nombre exacto de tu animación de estar quieto
+        }
+
+        // Girar sprite dependiendo de la dirección
+        if (moveInput > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveInput < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
+    }
+}
+
+
